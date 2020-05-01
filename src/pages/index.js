5905e1d9b3616056addee8b83a5fb44a5832a488
A-Game <javascript@adamheldring.com>
2020-05-01 20:52:52 +0200

    bring in project data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,23 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { projects } from "../data/projects"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Portfolio – Resume" />
-    <h1>Landing</h1>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <SEO title="Portfolio – Resume" />
+      <h1>Landing</h1>
+      {projects.map(project => <h2 key={project.id}>{project.title}</h2>)}
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <Image />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
