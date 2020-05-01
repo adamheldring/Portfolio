c117e1da14f17ba5f7fee1ecb4cd3f4f9b4fa91b
A-Game <javascript@adamheldring.com>
2020-05-01 20:38:46 +0200

    minor fix to seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -28,7 +28,7 @@ function SEO({ description, lang, meta, title }) {
   const metaDescription = description || site.siteMetadata.description
 
   return (
-    <
+    <Helmet
       htmlAttributes={{
         lang,
       }}
