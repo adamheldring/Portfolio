8194a3766ff1767ce77fda38f0007fed242c4475
A-Game <javascript@adamheldring.com>
2020-05-02 06:51:22 +0200

    bring in images with graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
 import { projects } from "../data/projects"
 
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+  const { ahlogo, portrait } = data
   return (
     <Layout>
       <SEO title="Portfolio – Resume" />
@@ -14,6 +16,8 @@ const IndexPage = () => {
       {projects.map(project => <h2 key={project.id}>{project.title}</h2>)}
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
+        <Img fluid={ahlogo.childImageSharp.fluid} />
+        <Img fluid={portrait.childImageSharp.fluid} />
       </div>
       <Link to="/page-2/">Go to page 2</Link>
     </Layout>
@@ -21,3 +25,26 @@ const IndexPage = () => {
 }
 
 export default IndexPage
+
+export const imageQuery = graphql`
+  query {
+    ahlogo: file(relativePath: { eq: "ah_logo_black.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    portrait: file(
+      relativePath: {
+        eq: "ah_portrait.jpg"
+      }
+    ) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
